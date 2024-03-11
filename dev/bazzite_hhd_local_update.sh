50aeac365030bca401cbc71bddd9a5fddf8f2b3a
Aarron Lee <aarron-lee@users.noreply.github.com>
2024-03-11 19:00:57 -0400

    fix local hhd update script

--- dev/bazzite_hhd_local_update.sh
+++ dev/bazzite_hhd_local_update.sh
@@ -6,9 +6,11 @@ git pull
 
 # disable hhd_dev if already running
 sudo systemctl disable --now hhd_local.service
+sudo systemctl disable --now hhd@$USER.service
 
 ./venv/bin/pip install -e .
 
+# handle for SE linux
 sudo chcon -u system_u -r object_r --type=bin_t /var/home/$(whoami)/.local/bin/hhd/venv/bin/hhd
 
 sudo systemctl daemon-reload

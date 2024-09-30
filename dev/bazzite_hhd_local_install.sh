4d7d00ee86398b46569d4b7eb9a2772afb96d41f
Aarron Lee <aarron-lee@users.noreply.github.com>
2024-09-30 12:16:18 -0400

    document controller for dev

--- dev/bazzite_hhd_local_install.sh
+++ dev/bazzite_hhd_local_install.sh
@@ -5,6 +5,11 @@ if [ "$EUID" -eq 0 ]; then
     exit 1
 fi
 
+# horipad steam controller: 
+# edit ~/.steam/steam/config/config.vdf
+# add to SDL_GamepadBind
+# 060000000d0f00009601000000000000,Steam Controller (HHD),a:b0,b:b1,x:b2,y:b3,back:b6,guide:b8,start:b7,leftstick:b9,rightstick:b10,leftshoulder:b4,rightshoulder:b5,dpup:h0.1,dpdown:h0.4,dpleft:h0.8,dpright:h0.2,leftx:a0,lefty:a1,rightx:a3,righty:a4,lefttrigger:a2,righttrigger:a5,paddle1:b13,paddle2:b12,paddle3:b15,paddle4:b14,misc2:b11,misc3:b16,misc4:b17,crc:ea35,
+
 # disable built in bazzite service
 sudo systemctl disable --now hhd@$USER.service
 # disable built-in hhd on reboots

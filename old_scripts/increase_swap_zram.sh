fc0b88c4d21cc238b59533904c8f64b6154c876f
Aarron Lee <aarron-lee@users.noreply.github.com>
2025-09-24 20:19:37 -0400

    update zram script

--- old_scripts/increase_swap_zram.sh
+++ old_scripts/increase_swap_zram.sh
@@ -4,7 +4,8 @@ echo "increasing zram swap"
 
 sudo cat <<EOF > "/etc/systemd/zram-generator.conf"
 [zram0]
-zram-fraction=2.0
+zram-size=ram*2
+compression-algorithm=lz4
 EOF
 
 echo "done. reboot required"
